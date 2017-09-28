bb0a748a81764bc2cfc5abcac260d5a54f041b3c
Bernardo Gonzalez Riede <31217674+MemBernd@users.noreply.github.com>
2017-09-28 14:45:23 +0200

    working.

--- program/logger.erl
+++ program/logger.erl
@@ -17,7 +17,7 @@ loop(Clock, Queue)  ->
             loop(NewClock, NewQueue);
         stop ->
             io:format("Emptying Queue...~n"),
-            lists:foreach(fun({Time, From, Message}) ->
+            lists:foreach(fun({Time, From, Message, _}) ->
                     io:format("log: ~w ~w ~p~n", [Time, From, Message])
                 end,
                 lists:keysort(1, Queue)),
@@ -27,21 +27,18 @@ loop(Clock, Queue)  ->
 log(From, Time, Msg, Clock, Queue) ->
     %io:format("log: ~w ~w ~p~n", [Time, From, Msg]),
     NewClock = vect:update(From, Time, Clock),
-    %io:format("clock ~p~n", [NewClock]),
-    NewQueue = [{Time, From, Msg} | Queue],
+    %sorted the hold back queue based on the sum of all the values in the timestamp
+    Sum = lists:foldl(fun({_, X}, Res) -> Res +X end, 0, Time),
+    NewQueue = lists:keysort(4,[{Time, From, Msg, Sum} | Queue]),
     io:format("~nLooking up Queue With clock: ~p~n", [NewClock]),
     EndQueue = lists:foldl(fun(Elem, TempQueue) -> processQueue(Elem, NewClock, TempQueue) end, [], NewQueue),
     {NewClock, EndQueue}.
 
-processQueue({Time, From, Message}, Clock, Queue) ->
-    %io:format("checking: ~nTime: ~w~nFrom: ~w~nMessage: ~w~n", [Time, From, Message]),
-    %if Queue == [] ->
-    %        io:format("Found messages which can be printed.~n")
-    %end,
+processQueue({Time, From, Message, Sum}, Clock, Queue) ->
     case vect:safe(Time, Clock) of
         true ->
             io:format("log: ~w ~w ~p~n", [Time, From, Message]),
             Queue;
         false ->
-            [{Time, From, Message} | Queue]
+            [{Time, From, Message, Sum} | Queue]
     end.
